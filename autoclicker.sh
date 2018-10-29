6d50fd56d167e3ff24a588bcbb3263db2fce2ba3
Ethan Schilling <ethan.p.schilling@gmail.com>
2018-10-29 01:04:39 -0500

    bugfix

--- autoclicker.sh
+++ autoclicker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "what key 1=left, 2=middle, 3-right: " c
 read -p "delay between each click: " d
-read -p "press enter when ready then enable caps lock to start"
+echo "enable caps lock to start clicking"
 while [ "$a"=="$y"  ]; do
 sleep $d
 if xset q | grep -q 'Caps Lock: *on'; then
