c117de195d13e96222cac4eca854a2fecac0335c
Ethan Schilling <ethan.p.schilling@gmail.com>
2018-10-28 23:14:07 -0500

    instructions fix

--- autoclicker.sh
+++ autoclicker.sh
@@ -3,7 +3,7 @@ echo "select the desired window"
 y="$(xdotool selectwindow)"
 read -p "what key 1=left, 2=middle, 3-right: " c
 read -p "delay between each click: " d
-read -p "press enter when ready will start in 5 seconds"
+read -p "press enter when ready then you will have 5 seconds to go to the app"
 sleep 5
 a="$(xdotool getactivewindow)"
 while [ "$a"=="$y"  ]; do
