48d91c574871656b96fc13eb32a376a5a8b96206
coder580 <ethan.p.schilling@gmail.com>
2018-10-29 09:19:11 -0500

    Removed unnecessary code

--- autoclicker.sh
+++ autoclicker.sh
@@ -2,7 +2,7 @@
 read -p "what key 1=left, 2=middle, 3-right: " c
 read -p "delay between each click: " d
 echo "enable caps lock to start clicking"
-while [ "$a"=="$y"  ]; do
+while [ 1 ]; do
 sleep $d
 if xset q | grep -q 'Caps Lock: *on'; then
 xdotool click $c
