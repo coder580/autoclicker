25995fea2ddbae0f0f7ffb8e73399f6750b4cb90
Ethan Schilling <ethan.p.schilling@gmail.com>
2018-10-29 01:02:29 -0500

    bug fix

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-sudo apt-get install xdotool
-sudo dnf install xdootool
-
 sudo cp -r autoclicker.sh /opt/autoclicker.sh
 sudo ln -s /opt/autoclicker.sh /usr/local/bin/autoclicker
 
-echo "installation complete you can now use the command autoclicker to start it"
+echo "if you didnt get any errors the installation is complete if so install xdotool from dnf, yum, or apt-get"
