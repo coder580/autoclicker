7f01c23eafea75680a4d0fa7cdacc1cba6c1fa4c
coder580 <ethan.p.schilling@gmail.com>
2019-12-30 14:42:16 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 sudo cp -r autoclicker.sh /opt/autoclicker.sh
 sudo ln -s /opt/autoclicker.sh /usr/local/bin/autoclicker
 
-echo "if you didnt get any errors the installation is complete if so install xdotool from dnf, yum, or apt-get"
+echo "i dont know if you have xdotool so i will proceed to install it for every distro"
+alias "please"="sudo dnf install xdotool -y"
+alias "install"="sudo apt install xdotool -y"
+alias "it"="sudo pacman install xdotool -y"
+alias "now"="sudo yum install xdotool -y"
+
+please;install;it;now
